ef85fe8c17305b17d782a2fd496b5e36f5087e4e
James Gissendaner Jr <jamestgissendanerjr@gmail.com>
2014-03-02 17:45:02 -0500

    Project template

---
+++ archiver-template/app-name/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import url, patterns, include
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browseable API.
+urlpatterns = patterns('',
+)
--- archiver/settings.py
+++ archiver/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = (
     'rest_framework',
     'rest_framework_swagger',
     'archiver',
-    ## APIs go below.
 )
 
 MIDDLEWARE_CLASSES = (
