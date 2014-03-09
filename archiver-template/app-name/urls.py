06483089195f85f6951bdb0d63b44f109a6e945a
James Gissendaner <jamestgissendanerjr@gmail.com>
2014-03-09 08:13:33 +0000

    Fixed Cover, Removed Template

--- archiver-template/app-name/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import url, patterns, include
-
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browseable API.
-urlpatterns = patterns('',
-)
