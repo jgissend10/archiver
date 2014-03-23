5123f54052eb0278f5136a5706cb14dbca6d359a
James Gissendaner <jamestgissendanerjr@gmail.com>
2014-03-23 18:24:55 +0000

    Registration changes

--- archiver/decorators.py
+++ archiver/decorators.py
@@ -1,6 +1,13 @@
+from django.contrib.auth.forms import AuthenticationForm as auth_form
+from django.core.context_processors import csrf
+
 def dashboard_view(view):
     def inner(request, *args, **kwargs):
         context = {'request': request, 'user': request.user}
+        if not request.user.is_active:
+            context['form'] = auth_form(request)
+            context['next'] = '/dashboard'
+            context.update(csrf(request))
         kwargs['context'] = context
         return view(request, *args, **kwargs)
     return inner
